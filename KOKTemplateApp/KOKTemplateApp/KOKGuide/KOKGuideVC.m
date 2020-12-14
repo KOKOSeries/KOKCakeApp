f2eb827d1b36792b6217bfff36813afd388d63e1
naiyuanyou <75198905+naiyuanyou@users.noreply.github.com>
2020-12-13 17:24:21 -0800

    1.0

--- KOKTemplateApp/KOKTemplateApp/KOKGuide/KOKGuideVC.m
+++ KOKTemplateApp/KOKTemplateApp/KOKGuide/KOKGuideVC.m
@@ -6,8 +6,11 @@
 //
 
 #import "KOKGuideVC.h"
-
+#import "SDCycleScrollView.h"
+#import <Masonry/Masonry.h>
 @interface KOKGuideVC ()
+@property (strong,nonatomic) UIButton *skipButton;
+@property (assign,nonatomic) BOOL isCanSkip;
 
 @end
 
@@ -16,8 +19,70 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor redColor];
+    self.skipButton = [[UIButton alloc]init];
+    [self.skipButton setTitle:@"跳过" forState:UIControlStateNormal];
+    
+    NSArray *imageNames = @[@"h1.jpg",
+                            @"h2.jpg",
+                            @"h3.jpg",
+                            ];
+    NSArray *titles = @[@"新建交流QQ群：185534916 ",
+                        @"disableScrollGesture可以设置禁止拖动",
+                        @"感谢您的支持，如果下载的",
+                        ];
+ 
+    
+    SDCycleScrollView *cycleScrollView = [SDCycleScrollView cycleScrollViewWithFrame:CGRectMake(0,0,[UIScreen mainScreen].bounds.size.width,[UIScreen mainScreen].bounds.size.height) shouldInfiniteLoop:YES imageNamesGroup:imageNames];
+    cycleScrollView.titlesGroup = titles;
+    [self.view addSubview:cycleScrollView];
+    cycleScrollView.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    __weak __typeof(self)weakSelf = self;
+    cycleScrollView.clickItemOperationBlock = ^(NSInteger currentIndex) {
+        __strong __typeof(weakSelf)strongSelf = weakSelf;
+        if (strongSelf.isCanSkip) {
+            
+        }else{
+            return;
+        }
+        
+        if ([strongSelf.mkDelegate respondsToSelector:@selector(didClickCancel)]) {
+            [strongSelf.mkDelegate didClickCancel];
+        }
+    };
+    cycleScrollView.itemDidScrollOperationBlock = ^(NSInteger currentIndex) {
+        NSLog(@"%@",@(currentIndex));
+        __strong __typeof(weakSelf)strongSelf = weakSelf;
+        if (currentIndex == 2) {
+            strongSelf.isCanSkip = YES;
+        }
+    };
+    [self.view addSubview:self.skipButton];
+    [self.skipButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.equalTo(@(10));
+        make.top.equalTo(@(80));
+        make.width.equalTo(@(80));
+        make.height.equalTo(@(30));
+    }];
+    self.skipButton.layer.cornerRadius = 15;
+    self.skipButton.layer.masksToBounds = YES;
+    self.skipButton.layer.borderColor = [UIColor colorNamed:@"tintColor"].CGColor;
+    self.skipButton.layer.borderWidth = 0.5;
+    self.isCanSkip = NO;
+    [self.skipButton addTarget:self action:@selector(skipHander) forControlEvents:UIControlEventTouchUpInside];
+}
+- (void)skipHander{
+    if ([self.mkDelegate respondsToSelector:@selector(didClickCancel)]) {
+        [self.mkDelegate didClickCancel];
+    }
 }
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+    if (self.isCanSkip) {
+        
+    }else{
+        return;
+    }
+    
     if ([self.mkDelegate respondsToSelector:@selector(didClickCancel)]) {
         [self.mkDelegate didClickCancel];
     }
--- KOKTemplateApp/KOKTemplateApp/vc/HomeViewController.m
+++ KOKTemplateApp/KOKTemplateApp/vc/HomeViewController.m
@@ -8,6 +8,9 @@
 #import "HomeViewController.h"
 #import "YYFPSLabel.h"
 #import "SceneDelegate.h"
+#import "SDCycleScrollView.h"
+#import <Masonry/Masonry.h>
+#define introduceH  120
 @interface HomeViewController ()
 @property (nonatomic, strong) YYFPSLabel *fpsLabel;
 @end
@@ -16,21 +19,76 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    _fpsLabel = [YYFPSLabel new];
-//    _fpsLabel.frame = CGRectMake(200, 200, 50, 30);
-//    [_fpsLabel sizeToFit];
-//    [self.view addSubview:_fpsLabel];
-//    UIWindow *window2 = [[UIWindow alloc]initWithWindowScene:[SceneDelegate shared].windowScene];
-////
-////    UIWindowScene *windowScene2= [UIWindowScene alloc]initWithSession: connectionOptions:(nonnull UISceneConnectionOptions *)
-//    window2.frame = CGRectMake(200, 200, 200, 200);
-//    
-//    UIViewController *vc =     [[UIViewController alloc]init];
-//    vc.view.backgroundColor = [UIColor redColor];
-//    window2.rootViewController = vc;
-//    window2.windowLevel = 100000;
-//    window2.hidden = NO;
-//    [window2 makeKeyAndVisible];
+    UIScrollView *demoContainerView = [[UIScrollView alloc] initWithFrame:self.view.frame];
+    demoContainerView.contentSize = CGSizeMake(self.view.frame.size.width, 3*1200);
+    demoContainerView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:demoContainerView];
+    
+    
+    //  type  介绍
+    UIView *introduceView = [[UIView alloc]initWithFrame:CGRectMake(0, 0,[UIScreen mainScreen].bounds.size.width,introduceH)];
+    introduceView.backgroundColor = [UIColor whiteColor];
+    //
+    UILabel *introduceLabel  = [[UILabel alloc]init];
+    introduceLabel.text = @"Introduce";
+    [introduceView addSubview:introduceLabel];
+    [introduceLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.equalTo(introduceView);
+        make.left.equalTo(@(20));
+    }];
+    
+    NSArray *introduceImageNames = @[@"冰淇淋蛋糕.jpg",
+                                     @"布丁蛋糕.jpg",
+                                     @"海绵蛋糕.jpeg",
+                                     @"慕斯蛋糕.jpg",
+                                     @"戚风蛋糕.jpg",
+                                     @"巧克力蛋糕.jpg",
+                                     @"水果蛋糕.jpg",
+                                     @"鲜奶油蛋糕.jpg",
+                                     @"芝士蛋糕.jpg"
+    ];
+    NSArray *introduceTitles = @[@"冰淇淋蛋糕",
+                                 @"布丁蛋糕",
+                                 @"海绵蛋糕",
+                                 @"慕斯蛋糕",
+                                 @"戚风蛋糕",
+                                 @"巧克力蛋糕",
+                                 @"水果蛋糕",
+                                 @"鲜奶油蛋糕",
+                                 @"芝士蛋糕"
+                        ];
+    
+    
+    SDCycleScrollView *introduceCycleScrollView = [SDCycleScrollView cycleScrollViewWithFrame:CGRectMake([UIScreen mainScreen].bounds.size.width/2-introduceH,0,introduceH*2,introduceH) shouldInfiniteLoop:YES imageNamesGroup:introduceImageNames];
+    introduceCycleScrollView.titlesGroup = introduceTitles;
+    [introduceView addSubview:introduceCycleScrollView];
+    introduceCycleScrollView.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    introduceCycleScrollView.pageControlDotSize = CGSizeMake(3, 3);
+    __weak __typeof(self)weakSelf = self;
+    introduceCycleScrollView.clickItemOperationBlock = ^(NSInteger currentIndex) {
+        __strong __typeof(weakSelf)strongSelf = weakSelf;
+//        if (strongSelf.isCanSkip) {
+//
+//        }else{
+//            return;
+//        }
+//
+//        if ([strongSelf.mkDelegate respondsToSelector:@selector(didClickCancel)]) {
+//            [strongSelf.mkDelegate didClickCancel];
+//        }
+    };
+    introduceCycleScrollView.itemDidScrollOperationBlock = ^(NSInteger currentIndex) {
+        NSLog(@"%@",@(currentIndex));
+        __strong __typeof(weakSelf)strongSelf = weakSelf;
+//        if (currentIndex == 2) {
+//            strongSelf.isCanSkip = YES;
+//        }
+    };
+    [demoContainerView addSubview:introduceView];
+    
+    
+    
+    
 }
 
 @end
